475bfc5ca57ba9875ee1b966cbe713c08bd050a9
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-03-30 19:53:37 +0200

    Updated demo scripts

--- demo/facete-query.sh
+++ demo/facete-query.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 QUERY=$1
-LIMIT=${2:-10}
+LIMIT=${2:-100}
 
 mkdir -p target
 wget -O target/facete3.jar -c 'https://github.com/hobbit-project/facete3/releases/download/facete3-bundle-1.2.0-SNAPSHOT/facete3-bundle-1.2.0-SNAPSHOT-jar-with-dependencies.jar'
 
-BASE=`curl "http://cord19.aksw.org/nli?query=$QUERY&limit=$LIMIT" | jq -r '.results[].id' | awk '{ print "( <"$0"> )" }' | paste -sd " " - | awk '{ print "SELECT ?s { VALUES (?s) { " $0 " } }"  }'`
+BASE=`curl -G "http://cord19.aksw.org/nli" --data-urlencode "query=$QUERY" --data-urlencode "limit=$LIMIT" | jq -r '.results[].id' | awk '{ print "( <"$0"> )" }' | paste -sd " " - | awk '{ print "SELECT ?s { VALUES (?s) { " $0 " } }"  }'`
 
 echo "Created base concept: $BASE"
+
+echo "$BASE" > /tmp/tmp.sparql
 
 java -cp target/facete3.jar facete3 -c "$BASE" http://cord19.aksw.org/sparql
 
---
+++ demo/nil-query.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+QUERY="$1"
+LIMIT="$2"
+
+curl -G "http://cord19.aksw.org/nli" --data-urlencode "query=$QUERY" --data-urlencode "limit=$LIMIT" | jq -r '.results[].id'
+
