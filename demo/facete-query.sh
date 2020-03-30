d3f029a954bcaffa2c99c9592168cc3e2002374a
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-03-30 14:53:39 +0200

    Made demo script self-contained

--- demo/facete-query.sh
+++ demo/facete-query.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 
 QUERY=$1
 LIMIT=${2:-10}
+
+mkdir -p target
+wget -O target/facete3.jar -c 'https://github.com/hobbit-project/facete3/releases/download/facete3-bundle-1.2.0-SNAPSHOT/facete3-bundle-1.2.0-SNAPSHOT-jar-with-dependencies.jar'
 
 BASE=`curl "http://cord19.aksw.org/nli?query=$QUERY&limit=$LIMIT" | jq -r '.results[].id' | awk '{ print "( <"$0"> )" }' | paste -sd " " - | awk '{ print "SELECT ?s { VALUES (?s) { " $0 " } }"  }'`
 
 echo "Created base concept: $BASE"
 
-facete3 -c "$BASE" http://cord19.aksw.org/sparql
+java -cp target/facete3.jar facete3 -c "$BASE" http://cord19.aksw.org/sparql
+
