a93c7c01a85a7ebf81b28a7dc6e0e89f139d20fd
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-03-30 19:59:44 +0200

    prettified a long bash line

--- demo/facete-query.sh
+++ demo/facete-query.sh
@@ -6,7 +6,11 @@ LIMIT="${2:-100}"
 mkdir -p target
 wget -O target/facete3.jar -c 'https://github.com/hobbit-project/facete3/releases/download/facete3-bundle-1.2.0-SNAPSHOT/facete3-bundle-1.2.0-SNAPSHOT-jar-with-dependencies.jar'
 
-BASE=`curl -G "http://cord19.aksw.org/nli" --data-urlencode "query=$QUERY" --data-urlencode "limit=$LIMIT" | jq -r '.results[].id' | awk '{ print "( <"$0"> )" }' | paste -sd " " - | awk '{ print "SELECT ?s { VALUES (?s) { " $0 " } }"  }'`
+BASE=`curl -G "http://cord19.aksw.org/nli" --data-urlencode "query=$QUERY" --data-urlencode "limit=$LIMIT" |\
+  jq -r '.results[].id' |\
+  awk '{ print "( <"$0"> )" }' |\
+  paste -sd " " - |\
+  awk '{ print "SELECT ?s { VALUES (?s) { " $0 " } }"  }'`
 
 echo "Created base concept: $BASE"
 
